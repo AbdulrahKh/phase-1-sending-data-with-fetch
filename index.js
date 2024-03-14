6d5d095ed024d40ccf23842a554fd8bd441232e4
AbdulrahmanKh <abdul.rahkh19@gmail.com>
2024-03-14 09:18:42 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(nameValue, emailValue) {
+  const response = fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${nameValue}`,
+      email: `${emailValue}`,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML = `${data.id}`;
+    })
+    .catch((error) => (document.body.innerHTML = `Error: ${error}`));
+  return response;
+}
+
+submitData("Steve", "Steve@example.com");
